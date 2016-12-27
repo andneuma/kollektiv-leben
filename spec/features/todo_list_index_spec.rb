227ad6d47c1dbf74bd12ab0b17bb26b444194849
kjoscha <kjoscha@gmail.com>
2016-12-27 10:57:18 +0100

    todo list index spec

---
+++ spec/features/todo_list_index_spec.rb
@@ -0,0 +1,57 @@
+feature 'Todo list index', :js do
+  before do
+    create(:todo_list)
+    visit '/'
+  end
+
+  scenario 'shows todo lists' do
+    expect(page).to have_content('TestList')
+  end
+
+  scenario 'can create new todo list and shows it without redirect' do
+    save_new_list
+    expect(page).to_not have_content('Speichern')
+    expect(page).to have_content('NewTodoList')
+    expand_new_list
+    expect(page).to have_content('NewTodoList description')
+  end
+
+  scenario 'can create new todo item and shows it without redirect' do
+    expand_list
+    save_new_item
+    expect(page).to_not have_content('Speichern')
+    expect(page).to have_content('NewTodoItem')
+  end
+
+  private
+
+  def save_new_list
+    click_on 'New todo list'
+    sleep(0.1)
+    fill_in('Name', with: 'NewTodoList')
+    fill_in('Description', with: 'NewTodoList description')
+    click_on 'Save'
+    sleep(0.1)
+  end
+
+  def expand_new_list
+    find('.todo-list-name', text: 'NewTodoList').trigger('click')
+    sleep(1)
+  end
+
+  def save_new_item
+    click_on 'New item'
+    sleep(0.1)
+    fill_in('Name', with: 'NewTodoItem')
+    fill_in('Description', with: 'NewTodoItem description')
+    fill_in('Start', with: '2015-01-01')
+    fill_in('End', with: '2016-01-01')
+    click_on 'Save'
+    sleep(0.1)
+  end
+
+  def expand_list
+    find('.todo-list-name', text: 'TestList').trigger('click')
+    sleep(1)
+  end
+end
